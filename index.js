6c9231fb5a03440a8aaa062b2dae3fea45b63c24
Ignacio <dev.ignacio.h@gmail.com>
2021-06-25 19:54:34 -0400

    text SVG's

--- index.js
+++ index.js
@@ -23,3 +23,11 @@ svg
   .attr('y1', 20)
   .attr('y2', 120)
   .attr('stroke', 'red');
+svg
+  .append('text')
+  .attr('x', 20)
+  .attr('y', 200)
+  .attr('fill', 'gray')
+  .text('Hello, ninjas!')
+  // .attr('style', 'font-family: arial;')
+  .style('font-family', 'arial')
