13cddfb21461cec8fb3425311d32ac137cde6bf2
Ignacio <dev.ignacio.h@gmail.com>
2021-06-25 19:29:14 -0400

    appening elements

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const canvas = select('.canvas')
+const canvas = d3.select('.canvas');
+
+const svg = canvas.append('svg');
+
+// append shapes to svg container
+svg.append('rect');
+svg.append('circle');
+svg.append('line');
