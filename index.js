e57833dfb1b4b1183445c39358378903b21df897
Ignacio <dev.ignacio.h@gmail.com>
2021-06-26 16:32:07 -0400

    joining data to multiple elements

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const data = [
-  {width: 200, height: 100, fill: 'red'}
+  {width: 200, height: 100, fill: 'red'},
+  {width: 100, height: 50, fill: 'pink'},
+  {width: 50, height: 25, fill: 'blue'},
 ]
 
 const svg = d3.select('svg')
 
-const rect = svg.select('rect')
+const rect = svg.selectAll('rect')
   .data(data)
-  .attr('width', (d, i, n)=> {
-    console.log('data', data);
-    console.log('d', d);
-    console.log('i', i);
-    console.log('n', n);
-    return d.width
-  })
+  .attr('width', d => d.width)
   .attr('height', d => d.height)
   .attr('fill', d => d.fill)
+
+console.log(rect);
