154af25c7511ca33485bed86daf5f1e866f86475
Ignacio <dev.ignacio.h@gmail.com>
2021-06-26 16:51:38 -0400

    enter selection

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const data = [
-  {width: 200, height: 100, fill: 'red'},
-  {width: 100, height: 50, fill: 'pink'},
-  {width: 50, height: 25, fill: 'blue'},
-]
+  { width: 200, height: 100, fill: 'red' },
+  { width: 100, height: 50, fill: 'pink' },
+  { width: 50, height: 25, fill: 'blue' }
+];
 
-const svg = d3.select('svg')
+const svg = d3.select('svg');
 
-const rect = svg.selectAll('rect')
+// Join data to rects
+const rects = svg
+  .selectAll('rect')
   .data(data)
-  .attr('width', d => d.width)
-  .attr('height', d => d.height)
-  .attr('fill', d => d.fill)
 
-console.log(rect);
+// add attrs to rects already in the DOm
+rects
+  .attr('width', (d) => d.width)
+  .attr('height', (d) => d.height)
+  .attr('width', (d) => d.width)
+  .attr('height', (d) => d.height)
+  .attr('fill', (d) => d.fill);
+
+// append the enter selection to DOM
+rects
+  .enter()
+  .append('rect')
+  .attr('width', (d) => d.width)
+  .attr('height', (d) => d.height)
+  .attr('fill', (d) => d.fill);
+
+console.log(rects);
