084bf66ea939767dc92949483d93192be0c0ab96
Ignacio <dev.ignacio.h@gmail.com>
2021-07-19 23:05:29 -0400

    external data JSON

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-const data = [
-  { width: 200, height: 100, fill: 'red' },
-  { width: 100, height: 50, fill: 'pink' },
-  { width: 50, height: 25, fill: 'blue' }
-];
-
+// select svg container first
 const svg = d3.select('svg');
 
-// Join data to rects
-const rects = svg
-  .selectAll('rect')
-  .data(data)
+d3.json('planets.json').then((data) => {
+  const circs = svg.selectAll('circle').data(data);
 
-// add attrs to rects already in the DOm
-rects
-  .attr('width', (d) => d.width)
-  .attr('height', (d) => d.height)
-  .attr('width', (d) => d.width)
-  .attr('height', (d) => d.height)
-  .attr('fill', (d) => d.fill);
+  // add attrs to circles already in the DOM
+  circs.attr('cy', 200)
+    .attr('cx', (d) => d.distance)
+    .attr('r', (d) => d.radius)
+    .attr('fill', (d) => d.fill);
 
-// append the enter selection to DOM
-rects
-  .enter()
-  .append('rect')
-  .attr('width', (d) => d.width)
-  .attr('height', (d) => d.height)
-  .attr('fill', (d) => d.fill);
-
-console.log(rects);
+  // append the enter selection to the DOM
+  circs.enter()
+    .append('circle')
+      .attr('cy', 200)
+      .attr('cx', (d) => d.distance)
+      .attr('r', (d) => d.radius)
+      .attr('fill', (d) => d.fill);
+});
