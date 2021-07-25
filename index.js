9c758e729f68f058fa02bbd538460a8afd61d0b1
Ignacio <dev.ignacio.h@gmail.com>
2021-07-24 21:10:13 -0400

    min max & extent

--- index.js
+++ index.js
@@ -2,26 +2,27 @@
 const svg = d3.select('svg');
 
 d3.json('menu.json').then(data => {
-
+  const min = d3.min(data, d => d.orders)
+  const max = d3.max(data, d => d.orders)
+  const extent = d3.extent(data, d => d.orders)
+  
   const y = d3.scaleLinear()
-    .domain([0, 1000])
+    .domain([0, max])
     .range([0, 500]);
+
+  // const min = d3.min(data.map(item => item.orders))
+  // const max = d3.max(data.map(item => item.orders))
+  // const extent = d3.extent(data.map(item => item.orders))
+  
+  console.log(min);
+  console.log(max);
+  console.log(extent);
 
   const x = d3.scaleBand()
     .domain(data.map(item => item.name))
     .range([0, 500])
     .paddingInner(.05)
     .paddingOuter(.05)
-
-
-  // console.log(x('veg curry'));
-  // console.log(x('veg pasta'));
-  // console.log(x.bandwidth());
-  // console.log(data.map(item => item.name));
-
-  // console.log(y(400));
-  // console.log(y(00));
-  // console.log(y(900));
 
   // join the data to rects
   const rects = svg.selectAll('rect')
