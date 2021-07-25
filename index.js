0f59b489fc299f4c0517b50c40e3d7eb1472ee26
Ignacio <dev.ignacio.h@gmail.com>
2021-07-25 01:12:31 -0400

    inverting scales

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ d3.json('menu.json').then((data) => {
   const y = d3
     .scaleLinear()
     .domain([0, max])
-    .range([0, graphHeight]);
+    .range([graphHeight, 0]);
 
   const x = d3
     .scaleBand()
@@ -46,19 +46,20 @@ d3.json('menu.json').then((data) => {
 
   rects
     .attr('width', x.bandwidth)
-    .attr('height', (d) => y(d.orders))
+    .attr('height', (d) => graphHeight - y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d) => x(d.name));
+    .attr('x', (d) => x(d.name))
+    .attr('y', (d) => y(d.orders))
 
   // append the enter selection to the DOM
   rects
     .enter()
     .append('rect')
       .attr('width', x.bandwidth)
-      .attr('height', (d) => y(d.orders))
+      .attr('height', (d) => graphHeight - y(d.orders))
       .attr('fill', 'orange')
-      .attr('x', (d) => x(d.name));
-
+      .attr('x', (d) => x(d.name))
+      .attr('y', (d) => y(d.orders))
   // create and call the axes
   const xAxis = d3.axisBottom(x);
   const yAxis = d3.axisLeft(y);
