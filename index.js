42aa37532886e72bc564de5277951cc9026183d7
Ignacio <dev.ignacio.h@gmail.com>
2021-07-24 20:41:20 -0400

    band scales

--- index.js
+++ index.js
@@ -7,24 +7,36 @@ d3.json('menu.json').then(data => {
     .domain([0, 1000])
     .range([0, 500]);
 
-  console.log(y(400));
-  console.log(y(00));
-  console.log(y(900));
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 500])
+    .paddingInner(.05)
+    .paddingOuter(.05)
+
+
+  // console.log(x('veg curry'));
+  // console.log(x('veg pasta'));
+  // console.log(x.bandwidth());
+  // console.log(data.map(item => item.name));
+
+  // console.log(y(400));
+  // console.log(y(00));
+  // console.log(y(900));
 
   // join the data to rects
   const rects = svg.selectAll('rect')
     .data(data)
 
-  rects.attr('width', 50)
+  rects.attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', d => x(d.name));
 
   // append the enter selection to the DOM
   rects.enter()
     .append('rect')
-      .attr('width', 50)
+      .attr('width', x.bandwidth)
       .attr('height', d => y(d.orders))
       .attr('fill', 'orange')
-      .attr('x', (d, i) => i * 70)
+      .attr('x', d => x(d.name))
 })
